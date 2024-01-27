63151e239488232a5f8e7fa017f5da70ec396b71
Dima Oliinyk <vt221_ods@student.ztu.edu.ua>
2024-01-27 20:12:44 +0200

    Додав gitignore

---
+++ Sum_of_numbers/Sum_of_numbers/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Обрахунок суми двох чисел");
+Console.WriteLine("Введiть перше число");
+int x=int.Parse(Console.ReadLine());
+Console.WriteLine("Введiть друге число");
+int y = int.Parse(Console.ReadLine());
+int s = x + y;
+Console.WriteLine("Сума чисел: "+s);
